27037b29c1d5eb39ae13e4305f5f462504d48422
Khalid Al-Roubaie <Khalidmustafamish@gmail.com>
2024-11-01 12:18:23 +0000

    Board initialise method working

--- src/main/java/org/lbg/c4/Board.java
+++ src/main/java/org/lbg/c4/Board.java
@@ -1,4 +1,5 @@
 package org.lbg.c4;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Board {
@@ -9,12 +10,16 @@ public class Board {
         System.out.println("Please specify a board size between 3 and 10");
         while (size < 3 || size > 10) {
             try {
-                size=obj.nextInt();
-                if(size<3 || size>10){
-                    System.out.println("Please specify a board size between 3 and 10");
+                int result = obj.nextInt();
+                if(result < 3 || result > 10){
+                    System.out.println("The board size cannot be smaller than 3 or larger than 10");
+                } else
+                {
+                    size = result;
                 }
-            } catch (Exception e) {
-                System.out.println("Please enter an integer");
+            } catch (InputMismatchException e) {
+                System.out.println("Board size must be an integer value between 3 and 10");
+                obj.nextLine();
             }
         }
     }
